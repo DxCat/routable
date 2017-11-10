89be5bacaa3a7dff1fdd2f1a1fd7c687850b2c47
askaoru <askaoru@users.noreply.github.com>
2017-11-10 12:34:07 +0800

    Create RoutableLocalize.php

---
+++ src/Http/Middleware/RoutableLocalize.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Askaoru\Routable\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\App;
+
+class RoutableLocalize
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (defined('ROUTABLE_LOCALE')) {
+            App::setLocale(ROUTABLE_LOCALE);
+        }
+
+        return $next($request);
+    }
+}
